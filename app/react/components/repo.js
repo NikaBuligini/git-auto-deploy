59b766d256020af2b462f3036c84cc0a770d7633
Nikoloz Buligini <nbuligini11@gmail.com>
2016-08-25 00:05:30 +0400

    repo tabs

--- app/react/components/repo.js
+++ app/react/components/repo.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
 import $ from 'jquery'
 
 import Loading from '../partials/loading'
+
+Tabs.setUseDefaultStyles(false)
 
 export default React.createClass({
   getInitialState () {
@@ -25,8 +28,12 @@ export default React.createClass({
     this.serverRequest.abort()
   },
 
+  handleSelect(index, last) {
+    console.log('Selected tab: ' + index + ', Last tab: ' + last);
+  },
+
   render () {
-    let cls = 'gems card'
+    let cls = 'repo card'
 
     if (!this.state.isLoaded) {
       return (
@@ -36,8 +43,36 @@ export default React.createClass({
 
     return (
       <div className={cls}>
-        <h3>{this.state.repository.name}</h3>
-        <p>{this.state.repository.description}</p>
+        <Tabs
+          onSelect={this.handleSelect}
+          selectedIndex={2}
+          className="tabs-container"
+        >
+          <TabList className="tabs">
+            <Tab>Overview</Tab>
+            <Tab>Connect</Tab>
+            <Tab>Activity</Tab>
+            <Tab>Access</Tab>
+            <Tab>Settings</Tab>
+          </TabList>
+
+          <TabPanel>
+            <h3>{this.state.repository.name}</h3>
+            <p>{this.state.repository.description}</p>
+          </TabPanel>
+          <TabPanel>
+            <span>Connect to your github repository</span>
+          </TabPanel>
+          <TabPanel>
+            <span>Your activities</span>
+          </TabPanel>
+          <TabPanel>
+            <span>Collaborators</span>
+          </TabPanel>
+          <TabPanel>
+            <span>Settings</span>
+          </TabPanel>
+        </Tabs>
       </div>
     )
   }
