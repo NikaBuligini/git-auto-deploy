d1954cbf492e12b7d30f703ba6b86eeac0006cdd
Nikoloz Buligini <nbuligini11@gmail.com>
2016-09-09 22:51:35 +0400

    fix capitalization

--- app/react/routes.js
+++ app/react/routes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, IndexRoute, IndexRedirect } from 'react-router'
+import { Route, IndexRoute } from 'react-router'
 
 import Layout from './components/layout'
 import Dashboard from './containers/Dashboard'
@@ -7,16 +7,16 @@ import CreateNewApp from './components/create-app'
 import AppLayout from './components/app/app-layout'
 import Overview from './containers/Overview'
 import Connect from './containers/Connect'
-import Activity from './containers/activity'
-import Access from './containers/access'
-import Settings from './containers/settings'
+import Activity from './containers/Activity'
+import Access from './containers/Access'
+import Settings from './containers/Settings'
 
 export default (
   <Route path="/" component={Layout}>
     <IndexRoute component={Dashboard}/>
     <Route path="create" component={CreateNewApp} />
     <Route path="apps/:appName" component={AppLayout}>
-      <IndexRedirect to="overview" />
+      <IndexRoute path="overview" component={Overview} />
       <Route path="overview" component={Overview} />
       <Route path="connect" component={Connect} />
       <Route path="activity" component={Activity} />
